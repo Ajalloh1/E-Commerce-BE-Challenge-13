19e9016ab38c83b02e01baff205be6b927b566a1
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-02 16:47:38 -0400

    added in category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,13 +6,40 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(dbCatData => {
+      if (!dbCatData) {
+        res.status(404).json({ message: 'No categories found' });
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    });
 });
-
 router.post('/', (req, res) => {
   // create a new category
 });
@@ -26,3 +53,4 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+

33b23a88aa8f5e704d867a19b62f1f3650579ee7
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-02 17:29:23 -0400

    updating and deleting category data added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,7 +30,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbCatData => {
       if (!dbCatData) {
-        res.status(404).json({ message: 'No categories found' });
+        res.status(404).json({ message: 'Category id NOT found' });
         return;
       }
       res.json(dbCatData);
@@ -42,10 +42,38 @@ router.get('/:id', (req, res) => {
 });
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const data = await Category.create(req.body);
+    res.status(201).json(data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id);
+    const updateCategoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: `Category id NOT found: ${req.params.id}` });
+      return;
+    }
+
+    if (!updateCategoryData[0]) {
+      res.status(400).json({ message: `Data category not updated for id: ${req.params.id}` });
+      return;
+    };
+
+    res.status(200).json(updateCategoryData);
+  } catch {
+    res.status(500).json(err);
+  };
 });
 
 router.delete('/:id', (req, res) => {
